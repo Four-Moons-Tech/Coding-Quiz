a010b02f92b51bd5293aac22769b6ac9f9718283
Anna Moon <anna@fourmoonstech.com>
2023-10-14 13:54:55 -0500

    added startQuiz function, fixed the error in declaring var startButton

--- Assets/script.js
+++ Assets/script.js
@@ -9,17 +9,24 @@
 //display message correct or wrong
 //wrong answer will penalize your score/time by 10 seconds
 
-var quizQuestion = document.querySelectorAll(".quiz-question")
-var startButton=document.getElementById('#start');// var for start button using id
+var quizQuestion = document.querySelectorAll(".quiz-question");
+var startButton = document.querySelector('#start');// var for start button using id
+var secondsLeft;
 
 $(document).ready(function(){
     $(quizQuestion).hide()//Hiding all quiz questions classes//
 })
 
+function startQuiz() {//function to start the quiz
+  secondsLeft = 45;
+  setTime()
+}
+  
 
 var timeEl =document.querySelector('.timer-count')
-var secondsLeft = 60;
+
 //
+
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
@@ -36,10 +43,9 @@ function setTime() {
   }, 1000);
 }
 
-setTime();
+//setTime();
 
-var question1=document.getElementById('#question1')
-  question1 = function(){
-  $(question1).show()
-  }
-  startButton.addEventListener("click", question1.show())
+  
+  startButton.addEventListener("click", startQuiz); 
+
+  //resetButton.addEventListener("click", resetGame);
